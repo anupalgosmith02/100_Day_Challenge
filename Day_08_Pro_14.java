8021fa3e81581e7beda1c714cfa206cae8bac771
anupalgosmith02 <anupawatade0202@gmail.com>
2026-01-17 08:24:47 +0530

    Create Day_08_Pro_14.java
    100_Day_Challenage

---
+++ Day_08_Pro_14.java
@@ -0,0 +1,56 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.function.*;
+import java.util.regex.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+
+
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(bufferedReader.readLine().trim());
+
+        List<Integer> a = Stream.of(bufferedReader.readLine().trim().split(" "))
+                .map(Integer::parseInt)
+                .collect(toList());
+
+        int totalSwaps = 0;
+
+        // Bubble Sort
+        for (int i = 0; i < n; i++) {
+            int numberOfSwaps = 0;
+
+            for (int j = 0; j < n - 1; j++) {
+                if (a.get(j) > a.get(j + 1)) {
+                    // swap
+                    int temp = a.get(j);
+                    a.set(j, a.get(j + 1));
+                    a.set(j + 1, temp);
+
+                    numberOfSwaps++;
+                }
+            }
+
+            totalSwaps += numberOfSwaps;
+
+            if (numberOfSwaps == 0) {
+                break;
+            }
+        }
+
+        // Output
+        System.out.println("Array is sorted in " + totalSwaps + " swaps.");
+        System.out.println("First Element: " + a.get(0));
+        System.out.println("Last Element: " + a.get(n - 1));
+
+        bufferedReader.close();
+    }
+}
