3a896f901c8e4e29a34fca94dbb67bb66aa92f04
anupalgosmith02 <anupawatade0202@gmail.com>
2026-02-04 19:34:08 +0530

    Create Day_23_Pro_31.c

---
+++ Day_23_Pro_31.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+
+
+int main() 
+{
+    int a, b;
+    scanf("%d\n%d", &a, &b);
+  	// Complete the code.
+     char *words[] = {
+        "zero", "one", "two", "three", "four",
+        "five", "six", "seven", "eight", "nine"
+    };
+
+    for (int i = a; i <= b; i++) {
+        if (i >= 1 && i <= 9) {
+            printf("%s\n", words[i]);
+        } else {
+            if (i % 2 == 0)
+                printf("even\n");
+            else
+                printf("odd\n");
+        }
+    }
+
+    return 0;
+}
+
